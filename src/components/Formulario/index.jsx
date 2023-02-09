03c3a8f100496451a020fe7f3a60fcfff6ddd316
Diego Rodrigues <diego.rodrigues3007@gmail.com>
2023-02-09 09:56:18 -0300

    Criação do botão do formulario

--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -4,6 +4,7 @@ import "./Form.css";
 // IMPORT DE COMPONENTES
 import CampoTexto from "../CampoTexto";
 import ListaTimes from "../ListaTimes";
+import Botao from "../Botao";
 
 const Formulario = () => {
   const times = [
@@ -24,6 +25,9 @@ const Formulario = () => {
         <CampoTexto label="Cargo" placeholder="Digite o seu cargo" />
         <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
         <ListaTimes label="Times" itens={times} />
+        <Botao>
+          Criar Card
+        </Botao>
       </form>
     </section>
   );
